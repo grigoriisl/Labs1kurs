d048b295f128810169449db3984b91c018c38f32
grigoriisl <slyusarenko.grigory@mail.ru>
2025-09-26 22:35:09 +0300

    laba

---
+++ geom/geom_4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int len=8;
+    float v[len];
+    float c1,c2, sum=0,res=0;
+    int num =1;
+    for (int i =0; i< len ; i=i+2)
+    {
+        
+        cout << "Введите координату X " << num << " -ого элемента";
+        cin >> v[i];
+        cout << "Введите координату Y " << num << " -ого элемента";
+        cin >> v[i+1];
+        num = num+1;
+    }
+    c1=pow((v[3]-v[1]),2);
+    c2=pow((v[2]-v[0]),2);    
+    res=pow((pow((v[3]-v[1]),2)+pow((v[2]-v[0+i]),2)), 0.5);
+
+    cout << "Длина диагонали: " << res;
+    return 0;
+}
